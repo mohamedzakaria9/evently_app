0c9aa436976db6b01fc318d63a3ed3788673fdff
Mohamed Zakaria <116889612+mohamedzakaria9@users.noreply.github.com>
2025-11-03 15:15:50 +0200

    Replace Google Maps API key with placeholder
    Updated Google Maps API key placeholder in AppDelegate.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    GMSServices.provideAPIKey("AIzaSyDOZpNa5apcSH4ZIAcNlmuX3S4nN2-fD0c")
+    GMSServices.provideAPIKey("Replace with your API Key")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
